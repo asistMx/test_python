afd28b7bc009c4f32096b6097a733cb371d6feac
lquintanillab <lquintanillab@gmail.com>
2024-04-28 23:56:29 -0600

    first commit

---
+++ funciones_recursivas.py
@@ -0,0 +1,12 @@
+
+def recursiva(valor):
+    print(valor)
+    if valor == 0:
+        return 0
+    else:
+        new = valor + recursiva(valor - 1)
+        print(new)
+        return new
+    
+
+recursiva(5)
